c9a052b0f0748a9a388d567ebe8ab2c17f51b43b
Adam Šćevko <66312592+scevkoadam@users.noreply.github.com>
2022-03-08 19:01:57 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
 var box = document.querySelector('.box');
-
-// var newDiv = document.createElement('div');
-// newDiv.className = "newDiv";
-// box.appendChild(newDiv);
-
-// var text = '<div class="newDiv"></div>';
-// box.innerHTML = text;
-
 createTable();
-
-var boxes = document.querySelectorAll('.xo')
-var pom = 0;
+var boxes = document.querySelectorAll('.xo');
 var lines = [
   [boxes[0],boxes[1],boxes[2]],
   [boxes[3],boxes[4],boxes[5]],
@@ -20,11 +10,16 @@ var lines = [
   [boxes[2],boxes[5],boxes[8]],
   [boxes[0],boxes[4],boxes[8]],
   [boxes[2],boxes[4],boxes[6]]
-]
+];
+var pom = 0;
+var n = 0;
 
-for (var i = 0; i < boxes.length; i++) {
-  boxes[i].addEventListener('click',insertSymbol)
-}
+// var newDiv = document.createElement('div');
+// newDiv.className = "newDiv";
+// box.appendChild(newDiv);
+
+// var text = '<div class="newDiv"></div>';
+// box.innerHTML = text;
 
 function createTable() {
   var text = '';
@@ -34,16 +29,24 @@ function createTable() {
   box.innerHTML = text;
 }
 
+
+function run() {
+    for (var i = 0; i < boxes.length; i++) {
+      boxes[i].addEventListener('click',insertSymbol);
+    }
+}
+
 function insertSymbol() {
-  if (pom === 0) {
+  if (this.innerHTML=="" && pom === 0) {
     this.innerHTML = "X";
     pom = 1;
-  } else {
+  } else if (this.innerHTML=="") {
     this.innerHTML = "O";
     pom = 0;
   } // moguce pomocu ternarnog operatora
   // (symbol == "0") ? symbol ="X" : symbol = "O"
   //this.innerHTML = symbol
+  n++;
   checkLines();
 }
 
@@ -54,9 +57,27 @@ function checkLines() {
       for (var j = 0; j < line.length; j++) {
         line[j].style.background = "tomato";
       }
-    }
+      for (var i = 0; i < boxes.length; i++) {
+        boxes[i].removeEventListener('click',insertSymbol);
+      }
+      setTimeout(playAgain, 3000);
+    } 
+  }
+  if (n === 9) {
+    setTimeout(playAgain, 3000);
   }
 }
+
+function playAgain() {
+  let restart = false;
+  restart = confirm("Do you want to play again?\nOK - Yes\nCancel - No");
+  if (restart === true) {
+    restart = false;
+    location.reload();
+  }
+}
+
+run();
 // function checkLines() {
 //   if (boxes[0].innerHTML == boxes[1].innerHTML && boxes[0].innerHTML == boxes[2].innerHTML && boxes[0].innerHTML != "") {
 //     boxes[0].style.background = "tomato";
